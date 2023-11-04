ca16904d55e5956ef8b7af03d01dd513d114a04a
Kelvin <kmunyimbili@gmail.com>
2023-11-04 17:34:14 +0200

    COMPLETE GAME -JS game logict is complete with 2 modules and 1 factory -the CSS and HTML is complete, simple and clean

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gameBoardModule = (()=>{
-    const board = ['', '', '', '', '', '', '', '', ''];
+    let board = ['', '', '', '', '', '', '', '', ''];
 
     const resetBoard = () => {
         board = ['', '', '', '', '', '', '', '', ''];
@@ -31,8 +31,16 @@ const gameBoardModule = (()=>{
         }
         return false;
     }
+    const renderBoard = () => {
+        let iteration = 0;
+        const HTMLboard = document.querySelectorAll(".cell");
+        HTMLboard.forEach((cell) => {
+            cell.innerHTML = board[iteration];
+            iteration++
+        })
+    }
 
-    return {resetBoard, placeMarker, getCurrentBoardState, winConditionMet, isCellActive};
+    return {resetBoard, placeMarker, getCurrentBoardState, winConditionMet, isCellActive, renderBoard};
 })();
 
 const player = function(name, marker) {
@@ -47,31 +55,90 @@ const player = function(name, marker) {
 }
 
 const gameController = (() => {
-    const player1 = player("player1", "X");
-    const player2 = player("player2", "O");
+
+    const player1 = player("Player 1", "X");
+    const player2 = player("Player 2", "O");
+
     let roundNumber = 0;
-
     let currentPlayer = player1;
-    const _switchPlayer = () => currentPlayer == player1? player2: player1;
 
-    function _round(index) {
-        gameBoardModule.placeMarker(currentPlayer.getPlayerMarker(), index);
-        _switchPlayer();
-        roundNumber++;
-        return roundNumber;    
+    function resetGame() {
+        gameBoardModule.renderBoard();
+        document.querySelector(".cover").remove();
+        document.querySelector(".p2").innerHTML = '';
+        document.querySelector(".p1").innerHTML = player1.getPlayerName();
+        document.querySelector(".p1wins").innerHTML = "";
+        document.querySelector(".p2wins").innerHTML = "";
     }
 
-    function _gameOver() {
-
-    }
-
-    const playRound = (index) => {
-        if (gameBoardModule.isCellActive(index) && !(gameBoardModule.winConditionMet())) {
-            _round(index);
+    const _switchPlayer = () => {
+        currentPlayer = currentPlayer == player1? player2: player1;
+        if (currentPlayer == player2) {
+            document.querySelector(".p1").innerHTML = '';
+            document.querySelector(".p2").innerHTML = player2.getPlayerName();
+            return
         } else {
-            _gameOver();
+            document.querySelector(".p2").innerHTML = '';
+            document.querySelector(".p1").innerHTML = player1.getPlayerName();
+            return
         }
     }
+    function _gameOver() {
+        const createEl = (element, innerText = "") => {
+            let el = document.createElement(element);
+            el.innerHTML = innerText;
+            return el;
+        }
+        const btn = document.createElement("button");
+        btn.innerText = "PLAY AGAIN";
+        let h1_1 = createEl("h1", "GAME");
+        let h1_2 = createEl("h1", "OVER");
+        const div1 = createEl("div");
+        const div2 = createEl("div");
+        
+        div1.appendChild(h1_1);
+        div1.appendChild(btn);
+        div1.appendChild(h1_2);
+        div2.appendChild(div1);
+        
+        div2.classList.add('cover');
+        div1.classList.add('textDiv');
+        btn.addEventListener('click', resetGame);
+        
+        document.querySelector(".container").appendChild(div2);
+        
+        if (roundNumber >= 9) {
+            h1_1.innerHTML = "DRAW";
+            h1_2.innerHTML = "";
+            document.querySelector(".p1").innerHTML = player1.getPlayerName();
+            document.querySelector(".p2").innerHTML = player2.getPlayerName();
+            roundNumber = 0;
+            return;
+        }
+        
+        if (currentPlayer == player1) {
+            document.querySelector(".p1wins").innerHTML = "WON!";
+        } else {
+            document.querySelector(".p2wins").innerHTML = "WON!";
+        }
+        roundNumber = 0;
+    }
+    const playRound = (event) => {
+        const index = parseInt(event.target.id);
+        if (!gameBoardModule.isCellActive(index)) {return};
 
-    return {playRound}
+        gameBoardModule.placeMarker(currentPlayer.getPlayerMarker(), index);
+        gameBoardModule.renderBoard();
+        roundNumber++;
+        if (gameBoardModule.winConditionMet() || roundNumber >= 9) {
+            gameBoardModule.resetBoard();
+            _gameOver();
+            currentPlayer = player1;
+            return;
+        }
+        _switchPlayer(); 
+    }
+    return {playRound, resetGame}
 })();
+
+document.querySelector(".board").addEventListener('click', gameController.playRound);
